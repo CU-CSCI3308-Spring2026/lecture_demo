35d2ac1115895d40d7af26bdd4654fc3d264c8a7
SreeshaNath <31390313+SreeshaNath@users.noreply.github.com>
2026-02-04 16:47:08 -0700

    Enhance updatePrice function for dynamic pricing
    Refactor updatePrice function to handle dynamic drink sizes.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,22 @@
 let esp_prices = { small: "$1", medium: "$2", large: "$3" };
 
 function updatePrice(obj, pName) {
-  
+
+  let selection = obj.value.toLowerCase();
+  let pEs = document.querySelector(`#${pName}`);
+
+  switch (selection) {
+    case "small":
+      pEs.innerHTML = esp_prices.small;
+      break;
+    case "medium":
+      pEs.innerHTML = esp_prices.medium;
+      break;
+    case "large":
+      pEs.innerHTML = esp_prices.large;
+      break;
+    default:
+      pName.textContent = "incorrect selection";
+  }
 }
+// Try to rewrite this function such that it can reused to update the price for any drink on the menu based on size selected by the user.
